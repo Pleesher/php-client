b6a7b55c47176a0b6faeabefcd89841abd134412
Jérémy Touati <jeremy.touati@gmail.com>
2016-10-25 23:49:25 +0200

    Exceptions can now take a (string) error code as a second argument

--- src/Pleesher/Client/Exception/Exception.php
+++ src/Pleesher/Client/Exception/Exception.php
@@ -3,4 +3,19 @@ namespace Pleesher\Client\Exception;
 
 class Exception extends \Exception
 {
+	protected $error_code;
+
+	public function __construct($message = null, $error_code = null, $previous = null)
+	{
+		parent::__construct($message, null, $previous);
+		$this->error_code = $error_code;
+	}
+
+	public function __toString()
+	{
+		ob_start();
+		if (!empty($this->error_code))
+			echo ' (' . $this->error_code . ')';
+		return ob_get_clean();
+	}
 }
