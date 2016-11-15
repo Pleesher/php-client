9b49cdb63da16378bad84b7ad6ac30d96377b771
Jérémy Touati <jeremy.touati@gmail.com>
2016-11-15 20:59:39 +0100

    Changed constructor signature to allow an optional array of "error parameters"

--- src/Pleesher/Client/Exception/Exception.php
+++ src/Pleesher/Client/Exception/Exception.php
@@ -4,16 +4,23 @@ namespace Pleesher\Client\Exception;
 class Exception extends \Exception
 {
 	protected $error_code;
+	protected $error_parameters;
 
-	public function __construct($message = null, $error_code = null, $previous = null)
+	public function __construct($message, $error_code = null, array $error_parameters = array(), $previous = null)
 	{
 		parent::__construct($message, null, $previous);
 		$this->error_code = $error_code;
+		$this->error_parameters = $error_parameters;
 	}
 
 	public function getErrorCode()
 	{
 		return $this->error_code;
+	}
+
+	public function getErrorParameters()
+	{
+		return $this->error_parameters;
 	}
 
 	public function __toString()
